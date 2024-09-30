fba38ef38378532578383fa56ba9cbde1d4dac64
AKN-NAGARAJU <arushikumar.nagaraju@gmail.com>
2024-09-30 19:50:59 +0100

    Add files via upload

---
+++ Shapes.py
@@ -0,0 +1,18 @@
+import pgzrun
+WIDTH=400
+HEIGHT=400
+def draw():
+    screen.fill("light green")
+    r1=Rect(70,90,100,50)
+    screen.draw.rect(r1,"yellow")
+    sq1=Rect(200,300,100,100)
+    screen.draw.filled_rect(sq1,(20,100,50))
+
+    
+
+
+
+
+
+
+pgzrun.go()
