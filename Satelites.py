f88014296ed5f74161b7aa16417c69b7112dd33b
AKN-NAGARAJU <arushikumar.nagaraju@gmail.com>
2024-12-23 18:55:33 +0000

    Add files via upload

--- Satelites.py
+++ Satelites.py
@@ -1,15 +1,20 @@
 import pgzrun
 import random
+import time
 WIDTH=400
 HEIGHT=400
+startime=0
+totaltime=0
 current=0
 total=8
 salist=[]
 co=[]
+TITLE="Satelite Malfunction"
 for i in range(total):
     s=Actor("sat")
     s.pos=random.randint(40,WIDTH-40),random.randint(40,HEIGHT-40)
-    salist.append(s)
+    salist.append(s) 
+startime=time.time()
 def draw():
     screen.blit("gal",(0,0))
     for s in salist:
@@ -17,9 +22,10 @@ def draw():
         screen.draw.text(str(salist.index(s)),(s.x+20,s.y))
     for c in co:
         screen.draw.line(c[0],c[1],"white")
+    screen.draw.text(str(totaltime),(20,20))
     
 def on_mouse_down(pos):
-    global current
+    global current,co
     if current<total:
         if salist[current].collidepoint(pos):
             if current!=0:
@@ -28,7 +34,15 @@ def on_mouse_down(pos):
                 co.append([cpos,lpos])
                 print(co)
             current=current+1
+        else:
+            current=0
+            co=[]
 
+def update():
+    global totaltime
+    if current<total:
+        totaltime=round(time.time()-startime)
+    
 
 
 
@@ -41,14 +55,3 @@ def on_mouse_down(pos):
 
 
 pgzrun.go()
-        
-
-
-
-    
-
-
-
-
-
-
