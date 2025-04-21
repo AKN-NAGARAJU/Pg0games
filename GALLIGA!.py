8188cb34ba55e0e8a96f22c752d521bb82fc8f0e
AKN-NAGARAJU <arushikumar.nagaraju@gmail.com>
2025-04-21 18:57:09 +0100

    Update GALLIGA!.py

--- GALLIGA!.py
+++ GALLIGA!.py
@@ -7,6 +7,7 @@ bullets=[]
 galliga=Actor("galliga")
 galliga.pos=(WIDTH/2,HEIGHT-100)
 nemesis_group=[]
+gameover=False
 x=30
 y=30
 for i in range(3):  
@@ -25,16 +26,30 @@ def draw():
     screen.fill("midnightblue")
     screen.draw.text(str(score),(0,0))
     galliga.draw()
-    for row in nemesis_group:
-        for nemesis in row:
-            nemesis.draw()
-    for b in bullets:
-        b.draw()          
+     
+    if gameover==True:
+        screen.draw.text("GAME OVER",(0,200  ))     
 
+    else:
+        for row in nemesis_group:
+            for nemesis in row:
+                nemesis.draw()
+        for b in bullets:
+            b.draw() 
 
 def update(): 
     #pass # because we didnt have anything to put we put that.
-    global score,direction
+    global score,direction,gameover
+#check for enemys
+    enemies=False
+    for row in nemesis_group:
+        if len(row)>=0:
+            enemies=True
+    if enemies==False:
+        gameover=True
+    
+
+
     if keyboard.left and galliga.x>=0:
         galliga.x=galliga.x-2
     elif keyboard.right and galliga.x<=WIDTH:
@@ -45,6 +60,19 @@ def update():
             b.y=b.y-10
         else:
             bullets.remove(b)
+    
+    #moving the nemesis left,right,up,down
+    movedown=False
+    if enemies==True:
+        if nemesis_group[0][-1].x>=WIDTH or nemesis_group[0][0].x<=0:
+            direction=direction*-1
+            movedown=True
+    for row in nemesis_group:
+        for n in row:  
+            n.x=n.x+2*direction  
+            if movedown:
+                n.y=n.y+68
+    
     #collision of bullet and enemy
     for row in nemesis_group:
         for n in row:
@@ -53,15 +81,12 @@ def update():
                     row.remove(n)
                     bullets.remove(b)
                     score=score+1
-
-    #moving the nemesis left,right,up,down
-    if nemesis_group[0][-1].x>=WIDTH or nemesis_group[0][0].x<=0:
-        direction=direction*-1
-
+    #collision of galliga and enemy
     for row in nemesis_group:
         for n in row:
-            n.x=n.x+2*direction  
-            
+            if galliga.colliderect(n):
+                gameover=True
+    
             
 
 def on_key_down(key):
